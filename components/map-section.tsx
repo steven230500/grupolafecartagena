5b9556182592cef859bde4bfa54da7b022e0e51f
steven230500 <spatino.gu@gmail.com>
2025-09-28 13:44:18 -0500

    update map

--- components/map-section.tsx
+++ components/map-section.tsx
@@ -22,7 +22,7 @@ export function MapSection() {
           <CardContent>
             <div className="aspect-video w-full rounded-lg overflow-hidden border border-border">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3923.7234567890123!2d-75.5123456789!3d10.4123456789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTDCsDI0JzQ0LjQiTiA3NcKwMzAnNDQuNCJX!5e0!3m2!1ses!2sco!4v1234567890123!5m2!1ses!2sco"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3923.8!2d-75.5144!3d10.3997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0:0x0!2zMTDCsDIzJzU5LjAiTiA3NcKwMzAnNTIuMCJX!5e0!3m2!1ses!2sco!4v1630000000000!5m2!1ses!2sco"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
